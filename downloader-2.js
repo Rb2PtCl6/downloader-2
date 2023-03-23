7fab87a1c24b027b4fad1151a0cda1b57edb38af
Rb2PtCl6 <ggmetalgg2@gmail.com>
2023-03-23 22:36:46 +0200

    Update downloader-2.js

--- downloader-2.js
+++ downloader-2.js
@@ -1,5 +1,5 @@
 const { spawn }=require("child_process");
-const fs = require('fs');
+const { readFileSync } = require("fs");
 const { basename } = require("path");
 
 const additional_path=""; // "path/to/file/"
@@ -7,7 +7,7 @@ const source=additional_path+"source.txt";
 const video=additional_path+"video"+Date.now()
 
 function links_from_source(){
-    return (fs.readFileSync(source,'utf-8',function(err,data){
+    return (readFileSync(source,'utf-8',function(err,data){
         if (err) return;
         return data
     })).split('\r\n')
@@ -27,21 +27,15 @@ function downloader(videofolder ,url){
         console.log(`child process exited with code ${code}`);
     });
 }
-function getOptions(videofolder, url){
-    var options = new Array;
-    switch(new URL(url).hostname){
-        case "v.animethemes.moe":
-            options.push("-o",basename(url))
-            options.push("-P",videofolder+"a")
-            options.push(url)
-            return options
-        case "www.youtube.com":
-        case "youtu.be":
-        case "music.youtube.com":
-            options.push("-P",videofolder+"y")
-            options.push(url)
-            return options
-    }
+function getOptions(videofolder, url) {
+    var hostname = new URL(url).hostname;
+    const options = {
+        "v.animethemes.moe": ["-o", basename(url), "-P", videofolder + "a", url],
+        "www.youtube.com": ["-P", videofolder + "y", url],
+        "youtu.be": ["-P", videofolder + "y", url],
+        "music.youtube.com": ["-P", videofolder + "y", url],
+    };
+    return options[hostname] || [];
 }
 // main
 for (const video_url of links_from_source()){
